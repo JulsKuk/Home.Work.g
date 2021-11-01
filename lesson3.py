c18260126302970be7481788187f6adfc32eaca4
JulsKuk <kuksinskaya.jul@gmail.com>
2021-11-01 19:50:02 +0200

    error correction

--- lesson3.py
+++ lesson3.py
@@ -1,11 +1,12 @@
-user_data = (input("Введите любое что-то: "))
-line_data = ("Это строка в которую {} новую строку").format(user_data)
+user_data = input("Введите любое значение: ")
+
+line_data = "Это строка в которую {} новую строку".format(user_data)
 print(line_data)
-replacement_data = ("'Замена в строке'")
-line_change = ("Это строка в которую {} новую строку").format(replacement_data)
-print(line_change)
-print (len(line_change))
-if isinstance("строка", str):
+
+line_data = "Это строка в которую {} новую строку".format("замена в строке")
+print(line_data)
+print(len(line_data))
+if line_data.find("строка",4,10) != -1  :
     print("Да")
-
-
+else:
+    print("Нет")
